3ef84f4a5fa9fa9fcc9acd92e258b3cd601435de
Jonathan Pabon <jonathanpabon.dev@gmail.com>
2024-10-07 21:42:33 -0500

    basename added to browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import { ToastContainer } from "react-toastify";
 const App = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-slate-200 text-slate-800 dark:bg-slate-900 dark:text-slate-200">
-      <BrowserRouter>
+      <BrowserRouter basename="/bills">
         <Routes>
-          <Route path="bills/" element={<Home />} />
-          <Route path="bills/login" element={<Login />} />
-          <Route path="bills/register" element={<Register />} />
-          <Route path="bills/list" element={<PaymentContainer />} />
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="list" element={<PaymentContainer />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
