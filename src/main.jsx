bbbe3db6afc948db89a3f3f660797b082444fb6e
Jonathan Pabon <jonathanpabon.dev@gmail.com>
2025-03-15 15:17:57 -0500

    Fix: route base name

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/bills/">
+    <BrowserRouter basename="/Bills">
       <App />
     </BrowserRouter>
   </StrictMode>,
